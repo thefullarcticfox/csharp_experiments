0f7f89372cdd5f7d5856e02c546691815f7ccb53
Ivan <thefullarcticfox@users.noreply.github.com>
2021-03-12 17:18:10 +0300

    init crypto playground

---
+++ Crypto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Crypto
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
